932bd03288c82d40d2fa9f144e8d358ea65fe042
Sven Fink <sven.fink@piclife.de>
2018-03-21 15:53:45 +0100

    Add login form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
